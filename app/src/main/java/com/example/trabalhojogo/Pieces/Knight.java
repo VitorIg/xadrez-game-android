e60d555a41ed321a37a33c23a0f3fb5403b4b4ad
Vitor Honorio <vittorigor.honorio@gmail.com>
2023-07-08 15:01:38 -0300

    correçao movimento do cavalo

--- app/src/main/java/com/example/trabalhojogo/Pieces/Knight.java
+++ app/src/main/java/com/example/trabalhojogo/Pieces/Knight.java
@@ -4,38 +4,34 @@ import com.example.trabalhojogo.Coordinates;
 import com.example.trabalhojogo.Position;
 
 import java.util.ArrayList;
+public class Knight extends Piece{
 
-public class Knight extends Piece {
     public Knight(boolean white) {
         super(white);
     }
+
     @Override
     public ArrayList<Coordinates> getAllowedMoves(Coordinates coordinates, Position[][] board) {
         ArrayList<Coordinates> allowedMoves = new ArrayList<>();
-        int x = coordinates.getX();
-        int y = coordinates.getY();
+        int[][] knightMoves = { {-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1} };
 
-        addMove(x + 2, y - 1, allowedMoves, board);
-        addMove(x + 1, y - 2, allowedMoves, board);
-        addMove(x - 2, y - 1, allowedMoves, board);
-        addMove(x - 1, y - 2, allowedMoves, board);
-        addMove(x + 2, y + 1, allowedMoves, board);
-        addMove(x + 1, y + 2, allowedMoves, board);
-        addMove(x - 2, y + 1, allowedMoves, board);
-        addMove(x - 1, y + 2, allowedMoves, board);
+        for (int[] move : knightMoves) {
+            int targetX = coordinates.getX() + move[0];
+            int targetY = coordinates.getY() + move[1];
+
+            if (isValidMove(targetX, targetY, board)) {
+                allowedMoves.add(new Coordinates(targetX, targetY));
+            }
+        }
 
         return allowedMoves;
     }
-    private void addMove(int x, int y, ArrayList<Coordinates> allowedMoves, Position[][] board) {
-        if (isValidMove(x, y) && isMoveAllowed(x, y, board)) {
-            allowedMoves.add(new Coordinates(x, y));
+
+    private boolean isValidMove(int x, int y, Position[][] board) {
+        if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+            Piece targetPiece = board[x][y].getPiece();
+            return targetPiece == null || targetPiece.isWhite() != isWhite();
         }
-    }
-    private boolean isValidMove(int x, int y) {
-        return x >= 0 && x < 8 && y >= 0 && y < 8;
-    }
-    private boolean isMoveAllowed(int x, int y, Position[][] board) {
-        Piece piece = board[x][y].getPiece();
-        return piece == null || piece.isWhite() != board[x][y].getPiece().isWhite();
+        return false;
     }
 }
