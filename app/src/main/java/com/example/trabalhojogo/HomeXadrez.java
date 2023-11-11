46d34f27f86e794d57180dc66d451a8cc8f41138
Vitor Honorio <vittorigor.honorio@gmail.com>
2023-11-10 22:47:31 -0300

    remoçao botao

--- app/src/main/java/com/example/trabalhojogo/HomeXadrez.java
+++ app/src/main/java/com/example/trabalhojogo/HomeXadrez.java
@@ -16,13 +16,6 @@ public class HomeXadrez extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_xadrez);
 
-        findViewById(R.id.buttonHomeVelha).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), GameXadrez.class));
-                overridePendingTransition(R.anim.slide_xadrez_velha, R.anim.slide_velha_xadrez);
-            }
-        });
 
         buttonGameXadrez = findViewById(R.id.button_jogar);
         buttonGameXadrez.setOnClickListener(view -> {
